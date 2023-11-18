79f3a21e795df3f45e9f02b004ab4b8c87e85e5c
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-18 11:04:44 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,51 @@
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+    return data
+    main()
+    data = generate_random_data()
+import random
+    for item in data:
+if __name__ == "__main__":
+def main():
+        print(f"Random Number: {item}")
+
+}
+
+        }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        }
+    }
+    public static void main(String[] args) {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);import random
+def main():
+
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+
+    main()
+    data = generate_random_data()
+    for item in data:
+def generate_random_data():
+
+    for item in data:
+    data = generate_random_data()
+    main()
+
+def main():
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    return data
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
