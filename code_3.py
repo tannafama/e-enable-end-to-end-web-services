b2c392ae2112d90b41933052f44d1686792d2e3b
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-18 11:41:07 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+
+    for item in data:
+import random
+        print(f"Random Number: {item}")
+def generate_random_data():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+
+    main()
+
+def main():
